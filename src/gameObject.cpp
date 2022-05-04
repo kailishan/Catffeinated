b1f2c70e30b029c27513e852542155c16ae95866
kailishan <kailishan@icloud.com>
2022-05-03 17:21:22 -0700

    kibble code cleanup

--- src/gameObject.cpp
+++ src/gameObject.cpp
@@ -94,13 +94,6 @@ void gameObject::process(std::vector<gameObject> others, int index,
   }
   if (!isStatic)
     move(ftime);
-  else { // KIBBLE SPIN
-    glm::mat4 R = glm::rotate(glm::mat4(1), (float)ftime, glm::vec3(0.0f, 1.0f, 0.0f));
-    R = formRotationMatrix(ftime);
-    matrix = R;
-    //matrix = glm::rotate(glm::mat4(1.0f), (float)ftime, glm::vec3(0.0f, 1.0f, 0.0f));
-  }
-
 }
 
 glm::mat4 gameObject::formRotationMatrix(float frametime) {
